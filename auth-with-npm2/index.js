b1448157459706abf3b94eb4ec98a66fbba63c79
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-02-16 14:17:15 +0530

    Add files via upload

---
+++ auth-with-npm2/index.js
@@ -0,0 +1,7 @@
+// const { v4: uuidv4 } = require('uuid');
+// import {v4 as uuidv4} from 'uuid';
+import {v4 as uuidv4} from 'uuid';
+
+let para = document.querySelector('p');
+
+para.textContent = uuidv4();
